71effe6d32a1b6a10597876893a9949eba888a58
ashkan1996 <zarehadi2@gmail.com>
2019-01-15 13:53:25 +0330

    Add files via upload

---
+++ Dijkstra/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Dijkstra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.btnDijkstra = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(43, 261);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(92, 23);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(43, 235);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(92, 20);
+            this.txtNum.TabIndex = 0;
+            // 
+            // btnDijkstra
+            // 
+            this.btnDijkstra.Location = new System.Drawing.Point(43, 287);
+            this.btnDijkstra.Name = "btnDijkstra";
+            this.btnDijkstra.Size = new System.Drawing.Size(92, 23);
+            this.btnDijkstra.TabIndex = 2;
+            this.btnDijkstra.Text = "Dijkstra";
+            this.btnDijkstra.UseVisualStyleBackColor = true;
+            this.btnDijkstra.Click += new System.EventHandler(this.btnDijkstra_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 12);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(169, 217);
+            this.txtLog.TabIndex = 3;
+            this.txtLog.Text = "Please Enter n:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnEnter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 317);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnDijkstra);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.btnEnter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button btnDijkstra;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ Dijkstra/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dijkstra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[,] graph = null;
+        int n;
+
+        int minDistance(int[] dist, bool[] sptSet)
+        {
+            int min = int.MaxValue, min_index = -1;
+            for (int v = 0; v < n; v++)
+                if (sptSet[v] == false && dist[v] <= min)
+                {
+                    min = dist[v];
+                    min_index = v;
+                }
+            return min_index;
+        }
+
+        void printSolution(int[] dist, int n)
+        {
+            String s = "Vertex - Distance from Source" + Environment.NewLine;
+            for (int i = 0; i < n; i++)
+                s += (i + "  -  " + dist[i] + Environment.NewLine);
+
+            txtLog.Text += s;
+        }
+
+        void dijkstra(int src)
+        {
+            int[] dist = new int[n]; 
+            bool[] sptSet = new bool[n];
+            for (int i = 0; i < n; i++)
+            {
+                dist[i] = int.MaxValue;
+                sptSet[i] = false;
+            }
+            dist[src] = 0;
+            for (int count = 0; count < n - 1; count++)
+            {
+                int u = minDistance(dist, sptSet);
+                sptSet[u] = true;
+                for (int v = 0; v < n; v++)
+                    if ( !sptSet[v] 
+                        && graph[u, v] != 0 
+                        && dist[u] != int.MaxValue 
+                        && dist[u] + graph[u, v] < dist[v])
+                        dist[v] = dist[u] + graph[u, v];
+            }
+            printSolution(dist, n);
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (graph == null)
+                {
+                    n = Convert.ToInt32(txtNum.Text);
+                    graph = new int[n, n];
+                    txtLog.Text = "n = " + n + Environment.NewLine;
+                    txtLog.Text += "Please Enter i j size: " + Environment.NewLine;
+                    txtNum.Clear();
+                }
+                else
+                {
+                    string[] s = txtNum.Text.Trim().Split(' ');
+                    txtNum.Clear();
+                    int i = Convert.ToInt32(s[0]);
+                    int j = Convert.ToInt32(s[1]);
+                    int size = Convert.ToInt32(s[2]);
+                    graph[i, j] = size;
+                    graph[j, i] = size;
+                    txtLog.Text += i + " -> " + j + " = " + size + Environment.NewLine;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDijkstra_Click(object sender, EventArgs e)
+        {
+            int src;
+            try
+            {
+                src = Convert.ToInt32(txtNum.Text);
+            }
+            catch (Exception ex)
+            {
+                src = 0;
+            }
+            txtLog.Text = "source = " + src + Environment.NewLine;
+            dijkstra(src);
+        }
+
+    }
+}
